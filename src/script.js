56ac7240e17c7522627e58e865cfa274294930b9
Dominik Sauter <domesauter@yahoo.de>
2024-04-03 23:51:50 +0200

    responsive design

--- src/script.js
+++ src/script.js
@@ -1,30 +1,31 @@
 const greetings = {
-    "de": ["Hallo,", "ich bin Dominik"],
-    "ba": ["Servus,", "i bin da Dominik"],
-    "bw": ["Griaß Di,", "i benn dr Dominik"],
-    "en": ["Hi,", "I'm Dominik"],
-    "it": ["Ciao,", "Io sono Dominik"],
-    "es": ["¡Hola,", "Soy Dominik"],
-    "zh": ["你好,", "我是Dominik"],
-    "ru": ["Привет,", "Я - Доминик"],
-    "jp": ["こんにちは,", "私はドミニクです"]
-    "pt": ["Oi,", "Eu sou Dominik"],
-    "fr": ["Salut,", "Je suis Dominik"],
-    "tr": ["Merhaba,", "Ben Dominik'im"],
-    "ar": ["مرحبا,", "أنا دومينيك"],
-    "hi": ["नमस्ते,", "मैं डोमिनिक हूँ"],
-    "ko": ["안녕하세요,", "나는 도미닉입니다"]
+    "de": ["<span>Hallo,<br></span>", "ich bin Dominik"],
+    "ba": ["<span>Servus,<br></span>", "i bin da Dominik"],
+    "bw": ["<span>Griaß Di,<br></span>", "i benn dr Dominik"],
+    "en": ["<span>Hi,<br></span>", "I'm Dominik"],
+    "it": ["<span>Ciao,<br></span>", "Io sono Dominik"],
+    "es": ["<span>Hola,<br></span>", "Soy Dominik"],
+    "zh": ["<span>你好,<br></span>", "我是Dominik"],
+    "ru": ["<span>Привет,<br></span>", "Я - Доминик"],
+    "jp": ["<span>こんにちは,<br></span>", "私はドミニクです"],
+    "pt": ["<span>Oi,<br></span>", "Eu sou Dominik"],
+    "fr": ["<span>Salut,<br></span>", "Je suis Dominik"],
+    "tr": ["<span>Merhaba,<br></span>", "Ben Dominik'im"],
+    "ar": ["<span>مرحبا,<br></span>", "أنا دومينيك"],
+    "hi": ["<span>नमस्ते,<br></span>", "मैं डोमिनिक हूँ"],
+    "ko": ["<span>안녕하세요,<br></span>", "나는 도미닉입니다"]
 };
 
 function getRandomGreeting(language) {
     const greeting = greetings[language];
-    return `${greeting[0]}\n${greeting[1]}`;
+    return `${greeting[0]}${greeting[1]}`;
 }
 
 function updateGreeting() {
-    const language = Object.keys(greetings)[Math.floor(Math.random() * Object.keys(greetings).length)];
+    const languages = Object.keys(greetings);
+    const randomLanguage = languages[Math.floor(Math.random() * languages.length)];
     const greetingElement = document.getElementById('greeting');
-    greetingElement.textContent = getRandomGreeting(language);
+    greetingElement.innerHTML = getRandomGreeting(randomLanguage);
 }
 
 setInterval(updateGreeting, 10000);
