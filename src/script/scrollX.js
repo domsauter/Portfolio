b07b8453a68880510f033e5a92dd0af07951729f
Dominik Sauter <domesauter@yahoo.de>
2024-05-31 15:54:04 +0200

    projects responsive

--- src/script/scrollX.js
+++ src/script/scrollX.js
@@ -1,36 +1,28 @@
-let scrollContainer = document.querySelector(".projects-container");
-let leftButton = document.getElementById("leftButton");
-let rightButton = document.getElementById("rightButton");
-
-function getCardWidth() {
-    let projectCard = document.querySelector(".project-card");
-    return projectCard ? projectCard.offsetWidth + parseInt(window.getComputedStyle(scrollContainer).gap) : 350; 
-}
-
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY * 2; // Increase scroll speed
-    scrollContainer.style.scrollBeavior = "auto";
-});
-
-leftButton.addEventListener("click", () => {
-    scrollContainer.scrollBy({
-        left: -getCardWidth(),
-        behavior: 'smooth'
-    });
-});
-
-rightButton.addEventListener("click", () => {
-    scrollContainer.scrollBy({
-        left: getCardWidth(),
-        behavior: 'smooth'
-    });
-});
-
-// Adjust card width on window resize
-window.addEventListener('resize', () => {
-    let cardWidth = getCardWidth();
-    document.querySelectorAll('.project-card').forEach(card => {
-        card.style.flexBasis = `${cardWidth}px`;
-    });
-});
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      danamicBullets: true,
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+          },
+        950: {
+        slidesPerView: 3,
+        },
+      },
+  });
