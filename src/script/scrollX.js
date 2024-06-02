7f5963dbfd73bfd4a04d1b666d1fc96b2f7191ee
Dominik Sauter <domesauter@yahoo.de>
2024-06-02 16:56:39 +0200

    projects slides

--- src/script/scrollX.js
+++ src/script/scrollX.js
@@ -1,28 +1,26 @@
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 3,
-    spaceBetween: 25,
-    loop: true,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      danamicBullets: true,
-    },
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-        },
-        520: {
-            slidesPerView: 2,
-          },
-        950: {
-        slidesPerView: 3,
-        },
+    480: {
+        slidesPerView: 2,
       },
-  });
+    968: {
+    slidesPerView: 3,
+    },
+  },
+});
